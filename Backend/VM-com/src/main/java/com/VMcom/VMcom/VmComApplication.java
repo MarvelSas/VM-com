8a22a0cb6fd42f1e43fa67670fa9c52607b7e157
MarvelSas <marcinkk543@gmail.com>
2024-02-04 16:44:16 +0100

    Spring Boot project after initialization. First steps.

---
+++ Backend/VM-com/src/main/java/com/VMcom/VMcom/VmComApplication.java
@@ -0,0 +1,13 @@
+package com.VMcom.VMcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmComApplication.class, args);
+	}
+
+}
---
+++ Backend/VM-com/src/test/java/com/VMcom/VMcom/VmComApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VMcom.VMcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
