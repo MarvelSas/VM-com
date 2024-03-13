cdfbfb79a8a686855e187fab17f7ca589f54f886
kkolcz <vipciorr@gmail.com>
2024-03-13 17:47:01 +0100

    disable slideshow

--- Frontend/src/app/pages/landing-page/landing-page.component.ts
+++ Frontend/src/app/pages/landing-page/landing-page.component.ts
@@ -8,20 +8,20 @@ import { ProductsService } from 'src/app/shared/services/products.service';
   styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent implements OnInit {
-  public slides = [
-    {
-      src: 'https://assets-global.website-files.com/5c895551cef9097fb47eaaa6/5c89580457d74f47ce70f5e6_Screen-Shot-2018-10-22-at-2.51.25-PM.png',
-    },
-    {
-      src: 'https://i.ytimg.com/vi/G2tmDB_eMBw/maxresdefault.jpg',
-    },
-    {
-      src: 'https://hackaday.com/wp-content/uploads/2014/06/high-voltage-monitor-power-supply.jpg',
-    },
-    {
-      src: 'https://hackaday.com/wp-content/uploads/2023/08/crt-transplant-in-progress.png',
-    },
-  ];
+  // public slides = [
+  //   {
+  //     src: 'https://assets-global.website-files.com/5c895551cef9097fb47eaaa6/5c89580457d74f47ce70f5e6_Screen-Shot-2018-10-22-at-2.51.25-PM.png',
+  //   },
+  //   {
+  //     src: 'https://i.ytimg.com/vi/G2tmDB_eMBw/maxresdefault.jpg',
+  //   },
+  //   {
+  //     src: 'https://hackaday.com/wp-content/uploads/2014/06/high-voltage-monitor-power-supply.jpg',
+  //   },
+  //   {
+  //     src: 'https://hackaday.com/wp-content/uploads/2023/08/crt-transplant-in-progress.png',
+  //   },
+  // ];
 
   products: IProduct[] = [];
 
