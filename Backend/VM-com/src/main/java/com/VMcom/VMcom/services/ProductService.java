2b024061511159db746dd3c1cadc2964474dd228
MarvelSas <marcinkk543@gmail.com>
2024-03-27 21:05:04 +0100

    -Add logic for creating directory for picture.

--- Backend/VM-com/src/main/java/com/VMcom/VMcom/services/ProductService.java
+++ Backend/VM-com/src/main/java/com/VMcom/VMcom/services/ProductService.java
@@ -8,24 +8,46 @@ import com.VMcom.VMcom.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
+
 public class ProductService {
 
     private final ProductRepository productRepository;
     private final ProductCategoryRepository productCategoryRepository;
 
+    private final Path rootLocation = Paths.get("src/main/resources/static/uploaded-pictures");
+
+    public ProductService(ProductRepository productRepository, ProductCategoryRepository productCategoryRepository) {
+        this.productRepository = productRepository;
+        this.productCategoryRepository = productCategoryRepository;
+
+
+        // verify if storage can be initialized
+        try {
+            Files.createDirectories(rootLocation);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not initialize storage", e);
+        }
+
+    }
 
     public List<Product> getAllProducts(){
         return productRepository.findAll();
     }
 
     public List<ProductCategory> getAllProductCategories() {return  productCategoryRepository.findAll();}
+
+
+
 
 
     public boolean addProductCategory(ProductCategory productCategory){
@@ -41,7 +63,7 @@ public class ProductService {
 
     public boolean addProduct(Product product){
 
-        //TODO ADD verification if category already exist in database.
+
         Product newProduct = new Product(product.getName(),product.getDescription(),product.getPrice(),product.getPhotoUrl(),product.getAmount(),product.getProductCategory());
 
         productRepository.save(newProduct);
